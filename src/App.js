9f3d3697da411852e9e34b43daa80ec11d75763a
kokus <gren.konrad@gmail.com>
2019-07-31 19:15:11 +0200

    Render recipes based on state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './App.module.css';
 
+const fakeRecipes = [
+  { id: 1, name: 'Spaghetti' },
+  { id: 2, name: 'Onion Pie' },
+]
+
 function App() {
+
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    setRecipes(fakeRecipes)
+  }, [])
+
   return (
     <div className={styles.root}>
       <ul className={styles.list}>
-        <li className={styles.listItem}>Spaghetti</li>
-        <li className={styles.listItem}>Onion Pie</li>
+        {recipes.map(recipe => (
+          <li className={styles.listItem} key={recipe.id}>{recipe.name}</li>
+        ))}
         <button className={styles.button}>Add recipe</button>
       </ul>
     </div>
