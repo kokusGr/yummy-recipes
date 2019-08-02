756f5fead2685f7e39cdff89c174998a0c63962b
kokus <gren.konrad@gmail.com>
2019-08-02 15:19:48 +0200

    Updates

---
+++ precache-manifest.8b768a940b0a221c0f8363ebfb0560ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794aa6f9c94a172d9389d59d006762ef",
+    "url": "/yummy-recipes/index.html"
+  },
+  {
+    "revision": "945f0d6c3e5dbb19f0ee",
+    "url": "/yummy-recipes/static/css/main.4da17466.chunk.css"
+  },
+  {
+    "revision": "5da88d8f7ff1e792b428",
+    "url": "/yummy-recipes/static/js/2.6f3fcfff.chunk.js"
+  },
+  {
+    "revision": "945f0d6c3e5dbb19f0ee",
+    "url": "/yummy-recipes/static/js/main.4b094986.chunk.js"
+  },
+  {
+    "revision": "c19622047c6b42475bf5",
+    "url": "/yummy-recipes/static/js/runtime~main.b123fec7.js"
+  }
+]);
