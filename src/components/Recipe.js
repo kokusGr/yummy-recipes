32bf4bbcff5edbe3575cf1580953b6ba720bdb5a
kokus <gren.konrad@gmail.com>
2019-08-02 13:14:25 +0200

    Basic animation

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Button from './Button'
+import Transition from './Transition'
 
 import styles from './Recipe.module.css'
 
@@ -20,29 +21,29 @@ const Recipe = props => {
   return (
     <li className={styles.root}
       onClick={() => { toggleExpanded(recipe) }}>
-    <div className={`${styles.header} ${isExpanded ? styles.headerExpanded : ''}`}>
-      <h2 className={`${styles.title} ${isExpanded ? styles.titleExpanded : ''}`}>{recipe.name}</h2>
-      {isExpanded ? (
-        <>
-          <Button type="text" color="black" onClick={handleEditPressed}>Edit</Button>
-          <div className={styles.spacer} />
-          <Button type="text" color="red" onClick={handleDeletePressed}>Delete</Button>
-        </>
-      ): null}
-    </div>
-    {isExpanded ? (
-      <>
-        <div className={styles.ingredientsContainer}>
-          <h3 className={styles.ingredientsTitle}>Ingredients</h3>
-          <ul className={styles.ingredientsList}>
-            {recipe.ingredients.map(ingredient => (
-              <li className={styles.ingredientsListItem} key={ingredient}>{ingredient}</li>
-            ))}
-          </ul>
-        </div>
-      </>
-    ) : null}
-  </li>
+      <div className={`${styles.header} ${isExpanded ? styles.headerExpanded : ''}`}>
+        <h2 className={`${styles.title} ${isExpanded ? styles.titleExpanded : ''}`}>{recipe.name}</h2>
+        {isExpanded ? (
+          <>
+            <Button type="text" color="black" onClick={handleEditPressed}>Edit</Button>
+            <div className={styles.spacer} />
+            <Button type="text" color="red" onClick={handleDeletePressed}>Delete</Button>
+          </>
+        ) : null}
+      </div>
+      <Transition isActive={isExpanded} timeout={300}>
+        {activeClassName => (
+          <div className={`${styles.ingredientsContainer} ${activeClassName === 'enter' ? styles.ingredientsContainerEnter : null}`}>
+            <h3 className={styles.ingredientsTitle}>Ingredients</h3>
+            <ul className={styles.ingredientsList}>
+              {recipe.ingredients.map(ingredient => (
+                <li className={styles.ingredientsListItem} key={ingredient}>{ingredient}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </Transition>
+    </li>
   )
 }
 
---
+++ src/components/Transition.js
@@ -0,0 +1,27 @@
+import { useState, useEffect, useRef } from 'react'
+
+const Transition = ({ children, timeout, isActive }) => {
+  const [activeClassName, setActiveClassName] = useState(null)
+  const timer = useRef(null)
+
+  useEffect(() => {
+    if (!activeClassName && isActive) {
+      setActiveClassName('init')
+      timer.current && clearTimeout(timer.current)
+      timer.current = null
+
+      requestAnimationFrame(() => {
+        setActiveClassName('enter')
+      })
+    } else if (activeClassName && !isActive && !timer.current) {
+      setActiveClassName('exit')
+      timer.current = setTimeout(() => {
+        setActiveClassName(null)
+      }, timeout)
+    }
+  }, [isActive, timeout, activeClassName])
+
+  return activeClassName ? children(activeClassName) : null
+}
+
+export default Transition
